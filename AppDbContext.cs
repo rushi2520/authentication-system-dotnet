49710ff5b9444df4b6d5927f31fa4037082b6871
rushi2520 <108563556+rushi2520@users.noreply.github.com>
2026-04-21 13:02:08 +0530

    Data

---
+++ AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PracticeMVC_DB_Identitty.Models;
+using PracticeMVC_DB_Identitty.Data;
+
+namespace PracticeMVC_DB_Identitty.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) // Must be specific!
+            : base(options)
+        {
+        }
+    }
+}
